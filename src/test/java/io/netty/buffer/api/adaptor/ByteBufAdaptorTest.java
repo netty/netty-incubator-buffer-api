3e6eceb58a0960c94aa6141c27ff699e6e9b14c4
Chris Vest <christianvest_hansen@apple.com>
2021-03-24 11:21:00 +0100

    Make it possible to run all tests in Intellij as well
    The previous set-up was only working out for Maven.

--- src/test/java/io/netty/buffer/api/adaptor/ByteBufAdaptorTest.java
+++ src/test/java/io/netty/buffer/api/adaptor/ByteBufAdaptorTest.java
@@ -15,7 +15,6 @@
  */
 package io.netty.buffer.api.adaptor;
 
-import io.netty.buffer.AbstractByteBufTest;
 import io.netty.buffer.ByteBuf;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
